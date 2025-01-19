8a910629eab131d02d2cdbbc298425c27e2d00fd
Panu Ervamaa <panu.ervamaa@helsinki.fi>
2025-01-19 21:35:53 +0200

    Implement base template

--- ruokareseptit/home.py
+++ ruokareseptit/home.py
@@ -1,10 +1,13 @@
 """Homepage"""
 
-from flask import Blueprint, current_app
+from flask import Blueprint
+from flask import current_app
+from flask import render_template
 
 bp = Blueprint("home", __name__)
 
 @bp.route("/")
 def index():
     """Homepage"""
-    return f"Hello {current_app.config['HELLO_WHAT']}!"
+    hello_what = current_app.config["HELLO_WHAT"]
+    return render_template("home/index.html", hello_what=hello_what)
