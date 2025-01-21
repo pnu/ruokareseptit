70392c191f45d99b54539cf6fda68606dc52c595
Panu Ervamaa <panu.ervamaa@helsinki.fi>
2025-01-21 19:20:30 +0200

    Initial implementation for listing users own recipes

--- ruokareseptit/profile.py
+++ ruokareseptit/profile.py
@@ -3,6 +3,9 @@
 from flask import Blueprint
 from flask import render_template
 from flask import g
+from flask import url_for
+
+from .db import get_db
 
 bp = Blueprint("profile", __name__, url_prefix="/profile")
 
@@ -14,12 +17,20 @@ def index():
     return render_template("profile/index.html", **context)
 
 
-@bp.route("/recipes")
-def recipes():
+@bp.route("/recipes/", defaults={'page': 0})
+@bp.route("/recipes/<page>")
+def recipes(page):
     """Own recipes"""
-    context = {}
+    query = "SELECT * FROM recipes WHERE author_id = ? LIMIT 11 OFFSET ?"
+    cursor = get_db().execute(query, [g.user["id"], page * 10])
+    rows = cursor.fetchall()
+    context = {"recipes": rows}
+    if len(rows) > 10:
+        rows.pop()
+        context["next_page"] = url_for("profile.recipes", page=page + 1)
+    if page > 0:
+        context["prev_page"] = url_for("profile.recipes", page=page - 1)
     return render_template("profile/recipes.html", **context)
-
 
 @bp.route("/friends")
 def friends():
