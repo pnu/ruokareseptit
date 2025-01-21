21f7d5eec48a35c37e01e81f59a73c0799afa3ed
Panu Ervamaa <panu.ervamaa@helsinki.fi>
2025-01-21 12:21:26 +0200

    Unpack template context from dict

--- ruokareseptit/home.py
+++ ruokareseptit/home.py
@@ -1,5 +1,4 @@
 """Homepage"""
-
 from flask import Blueprint
 from flask import current_app
 from flask import render_template
@@ -10,4 +9,7 @@ bp = Blueprint("home", __name__)
 def index():
     """Homepage"""
     hello_what = current_app.config["HELLO_WHAT"]
-    return render_template("home/index.html", hello_what=hello_what)
+    context = {
+        "hello_what": hello_what
+    }
+    return render_template("home/index.html", **context)
