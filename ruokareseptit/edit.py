61b627aedcbb4e2c603a3a9f788e981518c1b39e
Panu Ervamaa <panu.ervamaa@helsinki.fi>
2025-01-25 13:52:36 +0200

    Add create recipe

--- ruokareseptit/edit.py
+++ ruokareseptit/edit.py
@@ -4,6 +4,10 @@ from flask import Blueprint
 from flask import render_template
 from flask import g
 from flask import url_for
+from flask import request
+from flask import session
+from flask import flash
+from flask import redirect
 
 from .db import get_db
 from .auth import login_required
@@ -32,7 +36,34 @@ def recipe(recipe_id: int):
 @login_required
 def create():
     """Create new recipe"""
-    context = {}
+    if request.method == "GET":
+        session.pop("create_recipe_title", None)
+        session.pop("create_recipe_summary", None)
+        return render_template("edit/create.html")
+
+    title = request.form["title"]
+    summary = request.form["summary"]
+    session["create_recipe_title"] = title
+    session["create_recipe_summary"] = summary
+
+    if title == "":
+        flash_error("Reseptin nimi on pakollinen.")
+    elif len(title) < 4 or not title.isalnum():
+        flash_error("Reseptin nimi ei ole vaatimusten mukainen.")
+    else:
+        recipe_id = insert_recipe(title, summary)
+        if recipe_id is None:
+            flash_error("Palvelussa on jo samanniminen resepti. Valitse toinen nimi.")
+        else:
+            flash("Uusi resepti on luotu.")
+            session.pop("create_recipe_title", None)
+            session.pop("create_recipe_summary", None)
+            return redirect(url_for("edit.recipe", recipe_id=recipe_id))
+
+    context = {
+        "title": session.pop("create_recipe_title", ""),
+        "summary": session.pop("create_recipe_summary", "")
+    }
     return render_template("edit/create.html", **context)
 
 
@@ -41,3 +72,23 @@ def settings():
     """Own settings"""
     context = {}
     return render_template("edit/settings.html", **context)
+
+
+# Utility functions
+
+def flash_error(message: str):
+    """Flash form validation error"""
+    flash(message, "form_validation_error")
+
+def insert_recipe(title: str, summary: str) -> int | None:
+    """Insert new recipe to database. Return id if successful,
+    None otherwise."""
+    try:
+        query = "INSERT INTO recipes (title, summary, author_id) VALUES (?, ?, ?)"
+        params = (title, summary, session["uid"])
+        db = get_db()
+        res = db.execute(query, params)
+        db.commit()
+        return res.lastrowid
+    except db.IntegrityError:
+        return None
